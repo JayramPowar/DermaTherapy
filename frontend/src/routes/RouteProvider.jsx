64027a5db94503c02668cbb46b15df88849aa644
Jayram Powar <148804188+JayramPowar@users.noreply.github.com>
2026-01-04 10:17:35 +0530

    Delete frontend directory

--- frontend/src/routes/RouteProvider.jsx
+++
@@ -1,8 +0,0 @@
-import { Router } from "./Router";
-import { RouterProvider } from "react-router";
-
-export const RouteProvider = () => {
-    return (
-        <RouterProvider router={Router} />
-    )
-}
